0d4e1ead3b726c0e8893c2f52fbf1918d0470896
Nikolay Vasilev <n.vasilev.fmi@gmail.com>
2018-11-14 22:46:37 +0200

    add: codebase

---
+++ WPFCustomWIndow/WPFCustomWIndow/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using SourceWeave.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCustomWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : SWWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Some Magic");
+        }
+    }
+}
